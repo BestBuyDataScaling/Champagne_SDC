95961486a425448494697a6640bb58b750f74631
Danielle Blom <dlblom91@gmail.com>
2020-08-06 18:45:22 -0500

    Seed database with mock data

--- searchbar/database/index.js
+++ searchbar/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+const mock = require('./mockData');
+mongoose.connect('mongodb://localhost/bestbuy', {useNewUrlParser: true});
 
-const { Schema } = mongoose;
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -31,6 +31,50 @@ db.once('open', () => {
 
   // export function to create productModel class
   const ProductModel = mongoose.model('ProductModel', productSchema);
-  module.exports = ProductModel;
+
+  let saveToDatabase = (model) => {
+    let product = new ProductModel({
+      uniqueID: model.uniqueID,
+      name: model.name,
+      description: model.description,
+      brand: model.brand,
+      department: model.department,
+      color: model.color,
+      subDept: model.subDept,
+      sku: model.sku,
+      price: model.price,
+      avgRating: model.avgRating,
+      colors: model.colors,
+      reviews: [],
+      questions: {
+        question: model.questions.question,
+        answer: model.questions.answer
+      },
+      images: model.images,
+      peopleAlsoBought: [],
+      peopleAlsoViewed: [],
+      recentlyViewed: model.recentlyViewed
+    });
+    product.save();
+    console.log(`Created ${model.uniqueID}`)
+  }
+
+  let promiseData = mock.data.map(async (product) => {
+    return product
+  })
+
+  Promise.all(promiseData)
+  .then((products) => {
+    products.map((product) => {
+      saveToDatabase(product);
+    })
+  })
+  .catch(err => {
+    console.log(`Error saving product to database: ${err}`)
+  })
+
+  module.exports.productSchema = productSchema;
+  module.exports.ProductModel = ProductModel;
 
 });
+
