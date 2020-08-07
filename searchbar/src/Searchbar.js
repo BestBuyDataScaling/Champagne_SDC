f64e85bc062e2ae899188f1855917ef6245ef05a
Danielle Blom <dlblom91@gmail.com>
2020-08-07 15:47:51 -0500

    Complete fullstack, render searchbar with mock data options using material UI

--- searchbar/src/Searchbar.js
+++ searchbar/src/Searchbar.js
@@ -1,29 +1,53 @@
 import React from 'react';
 import axios from 'axios';
 import { TextField } from '@material-ui/core';
-import { Autocomplete } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
 
 class Searchbar extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-
+      products: []
     }
 
+    this.getProducts = this.getProducts.bind(this);
+  }
+
+  componentDidMount = () => {
+    this.getProducts();
+  }
+
+  getProducts = () => {
+    axios.get('http://localhost:3001/products')
+    .then(res => {
+      console.log(res);
+      this.setState({
+        products: res.data
+      })
+    })
+    .catch(err => {
+      console.log(`Axios error getting all products: ${err}`)
+    })
   }
 
   render() {
     return (
       <div>
-      {/* create a form for the searchbar */}
-      {/* the form will contain a text input */}
-      <form class="searchbar">
-        <input type="text" id="searchbarInput" maxlength="90" placeholder="Search Best Buy" autocomplete="off" spellcheck="false"></input>
-      </form>
-      {/* create a drop down */}
-      {/* the drop down will contain 10 options (max) that match what the user is typing and each option will be a link */}
-      </div>
+      <Autocomplete
+        id="searchbar"
+        options={this.state.products.map((product) => product.name)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder="Search Best Buy..."
+            margin="normal"
+            variant="outlined"
+            InputProps={{ ...params.InputProps, type: 'search' }}
+          />
+        )}
+      />
+    </div>
     )
   }
 }
