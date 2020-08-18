c235b5a5cd1f574a4377123b1ef20dcd2e810ba4
Danielle Blom <dlblom91@gmail.com>
2020-08-18 10:56:50 -0500

    Prepare for deployment with updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-3-129-15-180.us-east-2.compute.amazonaws.com',
+      host: 'ec2-3-15-179-73.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/tutorial.pem',
       ref: 'origin/master',
       repo: 'git@github.com:ATX-50-Team-Best-Buy/searchbar_service.git',
