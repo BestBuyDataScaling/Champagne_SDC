4aeaee58362b5666773f5e211556331de973b05d
Danielle Blom <dlblom91@gmail.com>
2020-08-20 16:45:13 -0500

    prepare to render footer to react DOM

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -9,57 +9,57 @@ import { Smartphone } from '@material-ui/icons';
 
 const Footer = (props) => {
   return (
-    <div className="footerContainer">
-      <div className="footer-bar">
-        <div className="footerBarContents">
+    <div className="db_footerContainer">
+      <div className="db_footer-bar">
+        <div className="db_footerBarContents">
           <div>Get the latest deals and more.</div>
           <input type="text" placeholder="Enter email address"/>
           <button>Sign Up</button>
-          <div className="mobileApp">
+          <div className="db_mobileApp">
             <IconButton >
               <Smartphone />
             Best Buy app</IconButton>
           </div>
         </div>
       </div>
-      <div className="lowerCenterFooter">
-        <div className="lowerCenterFooterContents">
-          <div className="footerLists">
-            <div className="col1">
-              <h3 className="colHeading
+      <div className="db_lowerCenterFooter">
+        <div className="db_lowerCenterFooterContents">
+          <div className="db_footerLists">
+            <div className="db_col1">
+              <h3 className="db_colHeading
               ">My Best Buy</h3>
-              <ul className="footerList">
+              <ul className="db_footerList">
                 <li>View Points & Certifications</li>
                 <li>Member Offers</li>
                 <li>Sign in or Create Account</li>
               </ul>
-              <h3 className="colHeading
+              <h3 className="db_colHeading
               ">Credit Cards</h3>
-              <ul className="footerList">
+              <ul className="db_footerList">
                 <li>Make a Payment at Citibank</li>
                 <li>Check Balance at Citibank</li>
                 <li>Credit Card Offers</li>
                 <li>Apply Now</li>
               </ul>
             </div>
-            <div className="col2">
-              <h3 className="colHeading
+            <div className="db_col2">
+              <h3 className="db_colHeading
               ">Orders & Returns</h3>
-                <ul className="footerList">
+                <ul className="db_footerList">
                   <li>Order Status</li>
                   <li>Shipping, Delivery & Store Pickup</li>
                   <li>Return & Exchange Promise</li>
                   <li>Price Match Guarantee</li>
                 </ul>
-                <h3 className="colHeading
+                <h3 className="db_colHeading
                 ">Lease to Own</h3>
                 <ul className="footerList">
                   <li>Apply Now</li>
                 </ul>
             </div>
-            <div className="col3">
-              <h3 className="colHeading">Support & Services </h3>
-                <ul className="footerList">
+            <div className="db_col3">
+              <h3 className="db_colHeading">Support & Services </h3>
+                <ul className="db_footerList">
                   <li>Customer Service</li>
                   <li>Protection Plans</li>
                   <li>Schedule a Service</li>
@@ -67,19 +67,19 @@ const Footer = (props) => {
                   <li>Product Recalls</li>
                 </ul>
             </div>
-            <div className="col4">
-              <h3 className="colHeading
+            <div className="db_col4">
+              <h3 className="db_colHeading
               ">Partnership</h3>
-              <ul className="footerList">
+              <ul className="db_footerList">
                 <li>Affiliate Program</li>
                 <li>Developers</li>
                 <li>Best Buy Education</li>
                 <li>Best Buy for Business</li>
                 <li>Advertise with Us</li>
               </ul>
-              <h3 className="colHeading
+              <h3 className="db_colHeading
               ">International Sites</h3>
-              <ul className="footerList">
+              <ul className="db_footerList">
                 <li>Best Buy Mexico</li>
                 <li>Best Buy Canada</li>
               </ul>
@@ -87,10 +87,10 @@ const Footer = (props) => {
           </div>
         </div>
         <hr />
-        <div className="row1">
+        <div className="db_row1">
           <p>Corporate Information | Forums, Blog & More | Careers | Accessibility | Terms & Conditions | Privacy | Interest-Based Ads | California Privacy Rights | Do Not Sell My Personal Information | California Supply Chain Transparency Act</p>
         </div>
-        <div className="socialMediaIcons">
+        <div className="db_socialMediaIcons">
           <IconButton>
             <Facebook />
           </IconButton>
@@ -104,7 +104,7 @@ const Footer = (props) => {
             <Pinterest />
           </IconButton>
         </div>
-        <div className="row2">
+        <div className="db_row2">
           <p>Prices and offers are subject to change. © 2020 Best Buy. All rights reserved. BEST BUY, the BEST BUY logo, the tag design, MY BEST BUY, and BESTBUY.COM are trademarks of Best Buy and its affiliated companies.</p>
         </div>
       </div>
