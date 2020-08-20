d91c4c96d01c15b12d260126b038788434c40ba1
Danielle Blom <dlblom91@gmail.com>
2020-08-20 12:42:34 -0500

    Transfer database and server into rebuild

---
+++ database/index.js
@@ -0,0 +1,78 @@
+const mock = require('./mockData');
+mongoose.connect('mongodb://localhost/bestbuy', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+  console.log('Hello from the Database!');
+
+  const productSchema = mongoose.Schema({
+    uniqueID: Number,
+    name: String,
+    description: String,
+    brand: String,
+    department: String,
+    color: String,
+    subDept: String,
+    sku: Number,
+    price: Number,
+    avgRating: Number,
+    colors: [],
+    reviews: Array,
+    questions: { question: String, answer: String },
+    images: [],
+    peopleAlsoBought: [],
+    peopleAlsoViewed: [],
+    recentlyViewed: Boolean
+  })
+
+  // enable text index in schema for search query
+  productSchema.index({ name: 'text'})
+
+  let Product = mongoose.model('Product', productSchema);
+
+  let saveToDatabase = (model) => {
+    let product = new Product({
+      uniqueID: model.uniqueID,
+      name: model.name,
+      description: model.description,
+      brand: model.brand,
+      department: model.department,
+      color: model.color,
+      subDept: model.subDept,
+      sku: model.sku,
+      price: model.price,
+      avgRating: model.avgRating,
+      colors: model.colors,
+      reviews: [],
+      questions: {
+        question: model.questions.question,
+        answer: model.questions.answer
+      },
+      images: model.images,
+      peopleAlsoBought: [],
+      peopleAlsoViewed: [],
+      recentlyViewed: model.recentlyViewed
+    });
+    // product.save();
+    // console.log(`Created ${model.uniqueID}`)
+  }
+
+  // let promiseData = mock.data.map(async (product) => {
+  //   return product
+  // })
+
+  // Promise.all(promiseData)
+  // .then((products) => {
+  //   products.map((product) => {
+  //     saveToDatabase(product);
+  //   })
+  // })
+  // .catch(err => {
+  //   console.log(`Error saving product to database: ${err}`)
+  // })
+
+  module.exports.Product = Product;
+})
