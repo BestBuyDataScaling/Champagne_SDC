cc396326b869233c278971881bda23e2767d12e2
Danielle Blom <dlblom91@gmail.com>
2020-08-20 16:45:13 -0500

    prepare to render footer to react DOM

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
+import Footer from "./Footer";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("searchbar_app")
-);
+ReactDOM.render(<App />, document.getElementById("searchbar_app"));
+
+ReactDOM.render(<Footer />, document.getElementById("footer"));
