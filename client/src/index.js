0559f3d1c8bae4d0d5bcdc7c24d8e56404482830
Danielle Blom <dlblom91@gmail.com>
2020-08-22 15:54:34 -0500

    Remove footer

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 import App from "./App";
-import Footer from "./components/Footer";
 
 ReactDOM.render(<App />, document.getElementById("searchbar_app"));
-
-ReactDOM.render(<Footer />, document.getElementById("footer"));
