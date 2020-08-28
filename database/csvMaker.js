17e9ca327cb43314b8de1299f7e0ee62f61ca3d1
Mike Champagne <geauxdrum@gmail.com>
2020-08-28 15:26:09 -0500

    makes 10M line csv file

---
+++ database/csvMaker.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+const writer = csvWriter();
+const faker = require('faker');
+const { data } = require('./mockData');
+
+
+// var counter = 1;
+
+// const dataGen = () => {
+//   writer.pipe(fs.createWriteStream('./data.csv'));
+//   for (var i = 0; i <= 1000000; i++) {
+//     writer.write({
+//       id: counter++,
+//       name: `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}`
+//     })
+//   }
+
+//   writer.end();
+//   console.log('done')
+// }
+
+// dataGen();
+// dataGen();
+const writeProducts = fs.createWriteStream('./data.csv');
+writeProducts.write('id,name\n', 'utf8');
+
+function writeTenMillionProducts(writer, encoding, callback) {
+  let i = 10000000;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const name = `${faker.commerce.productAdjective()} ${faker.commerce.productMaterial()} ${faker.commerce.product()}`
+      const data = `${id},${name}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        // see if we should continue, or wait
+        // don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      // had to stop early!
+      // write some more once it drains
+      writer.once('drain', write);
+    }
+  }
+  write()
+}
+
+
+writeTenMillionProducts(writeProducts, 'utf-8', () => {
+  writeProducts.end();
+});
